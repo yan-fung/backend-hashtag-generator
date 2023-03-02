d693ae22f406301ccd601f3fb1fc103d9b24aebc
Yan Fung <fungmingyan@hotmail.com>
2023-03-02 17:00:42 +0000

    Added Hashtag model and added User and Hashtag associations

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const UserModel = require('./user');
+const HashtagModel = require('./hashtag');
 
 const { PGDATABASE, PGUSER, PGPASSWORD, PGHOST, PGPORT } = process.env;
 
@@ -13,9 +14,16 @@ const setupDatabase = () => {
   });
 
   const User = UserModel(connection, Sequelize);
+  const Hashtag = HashtagModel(connection, Sequelize);
+
+  User.hasMany(Hashtag, {
+    foreignKey: {
+        allowNull: false,
+    }
+  });
 
   connection.sync({ alter: true });
-  return { User };
+  return { User, Hashtag };
 };
 
 module.exports = setupDatabase();
