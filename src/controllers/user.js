4e77bda78f11ff33b3e9e7712b126e8e7782ae13
Yan Fung <fungmingyan@hotmail.com>
2023-03-02 11:23:07 +0000

    Completed CRUD operation with controller functions for user endpoint

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,26 @@
 const { User } = require('../models');
 
+exports.getAllUsers = async (_req, res) => {
+  try {
+    const allUser = await User.findAll();
+
+    res.status(200).json(allUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+exports.getUserById = async (req, res) => {
+  const userId = req.params.id;
+  const user = await User.findByPk(userId);
+
+  if (!user) {
+    res.status(404).json({ message: `user ${userId} does not exist` })
+  } else {
+    res.status(200).json(user);
+  }
+}
+
 exports.createUsers = async (req, res) => {
   try {
     const newUser = await User.create(req.body);
@@ -9,5 +30,37 @@ exports.createUsers = async (req, res) => {
     const errorMessages = error.errors.map((e) => e.message);
 
     res.status(400).json({ errors: errorMessages });
+  }
+}
+
+exports.updateUserById = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const findUserById = await User.findByPk(userId);
+    if(!findUserById) {
+      res.status(404).json({ message: `user ${userId} does not exist` })
+    } else {
+      const updatedUser = await User.update(req.body, { where: { id: userId } });
+      res.status(200).json(updatedUser);
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+exports.deleteUserById = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const findUserById = await User.findByPk(userId);
+    if(!findUserById) {
+      res.status(404).json({ message: `user ${userId} does not exist` })
+    } else {
+      const deletedUser = await User.destroy({ where: { id: userId } });
+      res.status(200).json(deletedUser);
+    }
+  } catch (error) {
+    res.status(500).json(error);
   }
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,6 +4,10 @@ const userRouter = express.Router();
 
 const userController = require('../controllers/user');
 
+userRouter.get('/', userController.getAllUsers);
+userRouter.get('/:id', userController.getUserById);
 userRouter.post('/', userController.createUsers);
+userRouter.patch('/:id', userController.updateUserById);
+userRouter.delete('/:id', userController.deleteUserById);
 
 module.exports = userRouter;
