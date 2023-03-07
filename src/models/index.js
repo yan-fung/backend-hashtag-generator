3cc1906c4f4169b7d8746006bd54730ecabc76e5
Yan Fung <fungmingyan@hotmail.com>
2023-03-07 19:16:04 +0000

    Commented out the user table

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,4 @@
 const Sequelize = require("sequelize");
-const UserModel = require('./user');
 const HashtagModel = require('./hashtag');
 
 const { PGDATABASE, PGUSER, PGPASSWORD, PGHOST, PGPORT } = process.env;
@@ -13,17 +12,16 @@ const setupDatabase = () => {
     define: { timestamps: false },
   });
 
-  const User = UserModel(connection, Sequelize);
   const Hashtag = HashtagModel(connection, Sequelize);
 
-  User.hasMany(Hashtag, {
-    foreignKey: {
-        allowNull: false,
-    }
-  });
+  // User.hasMany(Hashtag, {
+  //   foreignKey: {
+  //       allowNull: false,
+  //   }
+  // });
 
   connection.sync({ alter: true });
-  return { User, Hashtag };
+  return { Hashtag };
 };
 
 module.exports = setupDatabase();
