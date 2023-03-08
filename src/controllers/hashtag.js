8b19be66379b3bef2ed686b89d091d0a5f453d99
Yan Fung <fungmingyan@hotmail.com>
2023-03-08 12:15:17 +0000

    Added routes and controllers function for getAllHadhtagsById and getSpecificHashtagsByUserId

--- src/controllers/hashtag.js
+++ src/controllers/hashtag.js
@@ -9,12 +9,35 @@ exports.getAllHashtags = async (_req, res) => {
   }
 }
 
+exports.getAllHashtagsById = async (req, res) => {
+  const userId = req.params.id;
+  const hashtag = await Hashtag.findAll({ where: { userId: userId}})
+
+  if(!hashtag) {
+    res.status(404).json({ message: `The hashtag could not be found.`})
+  } else {
+    res.status(200).json(hashtag);
+  }
+}
+
 exports.getHashtagByCategory = async (req, res) => {
   const specificCategory = req.params.category;
   const hashtag = await Hashtag.findAll({ where: { category: specificCategory}});
 
   if (!hashtag) {
-    res.status(404).json({ message: `Hashtag ${hashtagId} does not exist` })
+    res.status(404).json({ message: `The cateogry of ${specificCategory} does not exist` })
+  } else {
+    res.status(200).json(hashtag);
+  }
+}
+
+exports.getSpecificHashtagsByUserId = async (req, res) => {
+  const userId = req.params.id;
+  const specificCategory = req.params.category;
+  const hashtag = await Hashtag.findAll({ where: { category: specificCategory, userId: userId }});
+
+  if (!hashtag) {
+    res.status(404).json({ message: `The cateogry of ${specificCategory} does not exist` })
   } else {
     res.status(200).json(hashtag);
   }
--- src/routes/hashtag.js
+++ src/routes/hashtag.js
@@ -6,7 +6,9 @@ const hashtagController = require('../controllers/hashtag');
 
 hashtagRouter.post('/', hashtagController.createHashtag);
 hashtagRouter.get('/', hashtagController.getAllHashtags);
+hashtagRouter.get('/:id/hashtag', hashtagController.getAllHashtagsById);
 hashtagRouter.get('/:category', hashtagController.getHashtagByCategory);
+hashtagRouter.get('/:id/:category', hashtagController.getSpecificHashtagsByUserId);
 hashtagRouter.patch('/:id', hashtagController.updateHashtagById);
 hashtagRouter.delete('/:id', hashtagController.deleteHashtagById);
 
